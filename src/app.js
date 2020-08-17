91bdf20445c8030f6d135b6893345de99ec308d3
Marcelo Rondelli Brito <marcelo.brito@decision-one.net>
2020-08-17 10:15:59 -0300

    feito1

--- src/app.js
+++ src/app.js
@@ -2,8 +2,6 @@ const express = require("express");
 const cors = require("cors");
 const { uuid, isUuid } = require("uuidv4");
 
-// const { uuid } = require("uuidv4");
-
 const app = express();
 
 app.use(express.json());
